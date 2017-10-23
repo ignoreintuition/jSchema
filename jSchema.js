d881ba3ab34a80dc3db43ef65c6946fa764dcc7b
Brian Greig <brian@resurgencewebdesign.com>
2017-10-22 20:42:07 -0400

    Major update
    Added helper function for checking uniqueness of tables in schema.
    Bug fixes in the drop method
    Added update to refresh the data in a dataset in the schema
    Cleanup to remove all tables in the work namespace.

--- jSchema.js
+++ jSchema.js
@@ -2,8 +2,8 @@
   // 'use strict';
 
   function jSchema() {
-    var version = '0.1.1';
-    data = [],
+    var version = '0.2.0',
+      data = [],
       counter = 0,
       _schema = {
         tables: {},
@@ -14,16 +14,20 @@
     // @namespace jSchema
     // @method add
     // @param {Object} d - a dataset
-    // @param {Object} md - metadata about the dataset (name, primaryKey, forignKey)
+    // @param {Object} md - metadata about the dataset (name, primaryKey)
     _schema.add = function(d, metadata) {
       if (typeof d != "object") {
         console.log("Warning:" + d + " is not an object")
         return;
       }
-      var name = (metadata && metadata.name) ? metadata.name.toUpperCase() : "TABLE" + counter;
+      var name = (metadata && metadata.name) ? metadata.name.toUpperCase() : "TABLE" + counter++;
+      if (_checkUnique(name, this.tables) == false) {
+        console.log("Warning: " + name + " already exists in schema");
+        return 0;
+      }
       var self = this;
       this.tables[name] = {};
-      this.tables[name].id = counter;
+      this.tables[name].id = data.length;
       this.tables[name].pk = (metadata && metadata.primaryKey) ? metadata.primaryKey : null;
       this.tables[name].rows = d.length;
       this.tables[name].col = Object.keys(d[0]);
@@ -32,7 +36,6 @@
       });
       data.push(d);
       this.length = data.length;
-      counter++;
       return this;
     };
 
@@ -42,11 +45,8 @@
     // @param {String} d - dataset name
     _schema.get = function(d) {
       d = d.toUpperCase();
-      if (this.tables[d] === undefined) {
-        console.log("Warning: " + d + " does not exist in schema.");
-        return;
-      } else
-        return data[this.tables[d].id];
+      if (_checkForTable(d, this.tables) === false) return;
+      return data[this.tables[d].id];
     };
 
     // join two tables
@@ -57,12 +57,10 @@
     _schema.join = function(d1, d2) {
       d1 = d1.toUpperCase();
       d2 = d2.toUpperCase();
-      if (!self.tables[d1] || !self.tables[d2]) {
-        console.log("Warning: Table doesn't exist in schema")
-        return;
-      }
       var self = this,
         target = [];
+      if (_checkForTable(d1, this.tables) === false) return;
+      if (_checkForTable(d2, this.tables) === false) return;
       data[self.tables[d1].id].forEach(function(left) {
         data[self.tables[d2].id].forEach(function(right) {
           if (left[self.tables[d1].pk] == right[self.tables[d1].pk]) {
@@ -89,11 +87,13 @@
     // @param {String} d dataset
     _schema.drop = function(d) {
       d = d.toUpperCase();
-      if (this.tables[d] === undefined) {
-        console.log("Warning: " + d + " does not exist in schema")
-        return;
+      if (_checkForTable(d, this.tables) === false) return;
+      data.splice(this.tables[d].id, 1);
+      for (var key in this.tables) {
+        if (this.tables[key].id > this.tables[d].id) {
+          this.tables[key].id -= 1;
+        }
       }
-      data.splice(1, this.tables[d].id);
       delete this.tables[d];
       this.length = data.length;
       return this;
@@ -107,10 +107,7 @@
     // e.g. {name: 'height, order: 'des'}
     _schema.orderBy = function(d, attr) {
       d = d.toUpperCase();
-      if (this.tables[d] === undefined) {
-        console.log("Warning: " + d + " does not exist in schema")
-        return;
-      }
+      if (_checkForTable(d, this.tables) === false) return;
       attr.order = (attr.order != undefined && attr.order.toUpperCase() == 'ASC') ? 'ASC' : 'DESC';
       var orderByData = data[this.tables[d].id].sort(function(d1, d2) {
         return (attr.order == 'ASC') ? d1[attr.name] - d2[attr.name] : d2[attr.name] - d1[attr.name];
@@ -157,18 +154,36 @@
       //
     };
 
-    _schema.update = function() {
-      //
+    _schema.update = function(d, data) {
+      d = d.toUpperCase();
+      if (_checkForTable(d, this.tables) === false) return;
+      var pk = this.tables[d].pk
+      this.drop(d);
+      this.add(data, {
+        "name": d,
+        "primaryKey": pk
+      });
+      return this;
     };
 
-    _schema.cleanUp = function(){
-      //
-    }
+    // clean up everything that is in the work namespace
+    // @namespace jSchema
+    // @method cleanUp
+    _schema.cleanUp = function() {
+      for (var key in this.tables) {
+        if (key.indexOf('WORK.') > -1) {
+          this.drop(key);
+        }
+      }
+      return this;
+    };
 
     return _schema;
   };
 
-  //helper functions
+  //*********** helper functions ********************
+
+  // returns an array of distinct values
   function _distinct(d, v) {
     var unique = {}
     var arr = [];
@@ -180,6 +195,26 @@
     }
     return arr;
   };
+
+  // verifies that a table name is unique in the schema
+  function _checkUnique(d, a) {
+    for (var key in a) {
+      if (key == d) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // checks to ensure that a table exists in the schema
+  function _checkForTable(d, a) {
+    if (a[d] === undefined) {
+      console.log("Warning: " + d + " does not exist in schema.");
+      return false;
+    } else {
+      return true;
+    }
+  }
 
   if (typeof(window.jSchema) === 'undefined') {
     window.jSchema = jSchema();
